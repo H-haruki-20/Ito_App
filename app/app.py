059ebee55992842a88a495f69d5f81b2f9602153
H-haruki-20 <hisaharu4019@gmail.com>
2022-03-14 22:21:08 +0900

    flaskの実装1

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Flask, render_template, request
 
 # Flaskオブジェクトの生成
@@ -8,10 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/playmode",methods=["GET"])
+@app.route("/playmode",methods=['GET'])
 def play():
-    return render_template("play.html")
+    return render_template("play_number.html")
+
+@app.route("/playmode-number",methods=["POST"])
+def playmodenumberpost():
+    print("データを受け取りました．")
+    req = request.form["data1"]
+    return render_template("play_name.html",player_number = req)
+
+@app.route("/playmode-name",methods=["POST"])
+def playmodenamepost():
+    name = request.form["player1"]
+    return f"{name}さん，こんにちは！"
+
 
 #おまじない
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
